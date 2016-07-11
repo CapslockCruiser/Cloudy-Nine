d394137ff5634090710ccb8e0663a242c7d0df1d
capslockcruiser <william@Williams-MacBook-Air-2.local>
2016-07-11 12:13:59 -0500

    Pods added

---
+++ Pods/Target Support Files/Pods-Cloudy Nine/Pods-Cloudy Nine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Cloudy_Nine : NSObject
+@end
+@implementation PodsDummy_Pods_Cloudy_Nine
+@end
---
+++ Pods/Target Support Files/Pods-Cloudy Nine/Pods-Cloudy Nine-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_Cloudy_NineVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Cloudy_NineVersionString[];
+
